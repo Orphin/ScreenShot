bf61796dea731babcc0e10743b64d3c161edbc78
Amikura <32775699+Orphin@users.noreply.github.com>
2025-05-10 23:08:06 +0900

    Add files via upload

---
+++ get_position.ps1
@@ -0,0 +1,9 @@
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+Start-Sleep -s 3
+
+$X = [System.Windows.Forms.Cursor]::Position.X
+$Y = [System.Windows.Forms.Cursor]::Position.Y
+
+Write-Output "X: $X | Y: $Y"
+Read-Host
